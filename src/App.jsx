bd2673bc45e31e7fe27507440a0d2d12acc24741
davidetoscano129 <toscano.davidew@gmail.com>
2024-12-16 17:55:13 +0100

    feat: cards function wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import languages from './data/languages'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+
+        <header className="pt-6">
+          <h1>Learn Web Development</h1>
+        </header>
+
+        <main className="main">
+          <div className="pt-6">
+
+            {/* Buttons */}
+            {languages.map(curLanguage => <button key={curLanguage.id} className="btn btn-primary me-4">{curLanguage.title}
+            </button>)}
+
+            {/* Cards */}
+            {languages.map(curLanguage => <div key={curLanguage.id} className="card mt-5">
+              <div className="card-body">
+                <h3>{curLanguage.title}</h3>
+                <p className="card-text">{curLanguage.description}</p>
+              </div>
+            </div>)}
+
+          </div>
+        </main>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
